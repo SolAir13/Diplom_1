5d7dd184abe574331e08d8b137a6ac910df3eedf
Alexey Solomatin <asolomatin94@yandex.ru>
2024-01-30 20:46:40 +0300

    First commit Diplom_1

---
+++ src/test/java/praktikum/IngredientTest/IngredientTest.java
@@ -0,0 +1,35 @@
+package praktikum.IngredientTest;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTest {
+    private final String name = RandomStringUtils.randomAlphabetic(8);
+    private final Float price = Float.valueOf(RandomStringUtils.randomNumeric(3));
+    IngredientType type;
+
+    @Test
+    public void getPriceReturnEnteredPrice() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        float returnValue = ingredient.getPrice();
+        assertEquals(price, returnValue, 0.0);
+    }
+
+    @Test
+    public void getNameReturnEnteredName() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        String returnValue = ingredient.getName();
+        assertEquals(name, returnValue);
+    }
+
+    @Test
+    public void getTypeReturnEnteredType() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        IngredientType expectedType = ingredient.getType();
+        assertEquals(type, expectedType);
+    }
+}
